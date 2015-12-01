7dfe3325205593d4f117c78f0b4bba75adfcee31
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-01 09:33:54 -0500

    Kill ring stuff

--- lisp/init-misc.el
+++ lisp/init-misc.el
@@ -25,6 +25,12 @@
 
 (require 'etags-select)
 
+(autoload 'kill-ring-search "kill-ring-search"
+  "Search the kill ring in the minibuffer."
+  (interactive))
+
+(global-set-key "\M-\C-y" 'kill-ring-search)
+
 (defun jcm-edit-startup-file ()
   "Edit the startup file for emacs."
   (interactive)
