849ece0f2de46d8355f4780c6f5237c48791384c
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2018-01-23 09:45:41 -0500

    Javascript settings

--- customize-work.el
+++ customize-work.el
@@ -30,7 +30,7 @@
  '(org-todo-keywords (quote ((sequence "TODO(t)" "DONE(d)"))))
  '(package-selected-packages
    (quote
-    (autopair aggressive-indent jsx-mode yesql-ghosts smooth-scrolling diminish magit use-package undo-tree cider counsel which-key avy ivy solarized-theme esup swiper ecb dockerfile-mode rjsx-mode js2-mode react-snippets nav-flash rainbow-delimiters guide-key groovy-mode clojure-mode-extra-font-locking scala-mode yaml-mode wgrep-ag web-mode smex projectile paredit maker-mode macrostep less-css-mode ido-vertical-mode ido-ubiquitous ggtags flx-ido etags-select ensime company-quickhelp color-theme-solarized ag)))
+    (paredit-everywhere autopair aggressive-indent jsx-mode yesql-ghosts smooth-scrolling diminish magit use-package undo-tree cider counsel which-key avy ivy solarized-theme esup swiper ecb dockerfile-mode rjsx-mode js2-mode react-snippets nav-flash rainbow-delimiters guide-key groovy-mode clojure-mode-extra-font-locking scala-mode yaml-mode wgrep-ag web-mode smex projectile paredit maker-mode macrostep less-css-mode ido-vertical-mode ido-ubiquitous ggtags flx-ido etags-select ensime company-quickhelp color-theme-solarized ag)))
  '(projectile-tags-command "etags *.c")
  '(scala-indent:align-parameters t)
  '(solarized-distinct-fringe-background t)
@@ -40,6 +40,8 @@
  '(solarized-use-variable-pitch nil)
  '(tooltip-mode nil)
  '(visible-bell t)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
  '(which-key-popup-type (quote minibuffer))
  '(yas-snippet-dirs
    (quote
