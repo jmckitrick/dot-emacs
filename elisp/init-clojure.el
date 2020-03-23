299d7dd19e15ceea3d63808cf31bcc774deb238d
Jonathon McKitrick <jmckitrick@gmail.com>
2020-03-23 07:33:43 -0400

    Restore some other clojure settings

--- elisp/init-clojure.el
+++ elisp/init-clojure.el
@@ -14,4 +14,13 @@
    cider-prompt-save-file-on-load nil
    cider-repl-display-help-banner nil))
 
+(defun my-cider-repl-mode-hook ()
+  ;(company-mode 1)
+  ;(cider-eldoc-setup)
+  (use-package init-lisp)
+  (my-lisp-setup))
+
+;(add-hook 'cider-mode-hook #'my-cider-mode-hook)
+(add-hook 'cider-repl-mode-hook #'my-cider-repl-mode-hook)
+
 (provide 'init-clojure)
