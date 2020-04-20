5b381454ada1b70cd63e41da54dcf482c47b74d8
Jonathon McKitrick <jmckitrick@gmail.com>
2020-04-20 16:44:56 -0400

    Frog menu fonts

--- customize.el
+++ customize.el
@@ -29,4 +29,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(frog-menu-action-keybinding-face ((t (:inherit default :height 1.4))))
+ '(frog-menu-actions-face ((t (:inherit default :height 1.4))))
+ '(frog-menu-border ((t (:background "white" :height 1.4))))
+ '(frog-menu-candidates-face ((t (:inherit default :height 1.4))))
+ '(frog-menu-posframe-background-face ((t (:inherit default :height 1.4))))
+ '(frog-menu-prompt-face ((t (:inherit default :height 1.4)))))
