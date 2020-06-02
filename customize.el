1e9585472a019f0471a744c863b349fec3ef923d
Jonathon McKitrick <jmckitrick@gmail.com>
2020-06-02 05:20:02 -0400

    Local customizations

--- customize.el
+++ customize.el
@@ -3,22 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(avy-keys (quote (97 111 101 117 104 116 110 115)))
+ '(avy-keys '(97 111 101 117 104 116 110 115))
  '(c-basic-offset 4)
  '(cider-prompt-for-symbol nil)
  '(cider-repl-display-in-current-window nil)
  '(cider-repl-pop-to-buffer-on-connect nil)
  '(custom-safe-themes
-   (quote
-    ("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default)))
+   '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" default))
  '(kill-whole-line t)
- '(magit-display-buffer-function (quote magit-display-buffer-fullframe-status-v1))
- '(magit-visit-ref-behavior (quote (checkout-branch)))
+ '(magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+ '(magit-visit-ref-behavior '(checkout-branch))
  '(package-selected-packages
-   (quote
-    (ace-window frog-jump-buffer yasnippet-snippets rainbow-delimiters yaml-mode restclient markdown-mode rust-mode realgud realgud-lldb php-mode yasnippet projectile-ripgrep js2-mode coffee-mode robe haml-mode solarized-theme ag avy cider company-quickhelp counsel diminish etags-select macrostep magit nav-flash paredit projectile smex smooth-scrolling undo-tree use-package web-mode wgrep-ag which-key yesql-ghosts)))
- '(projectile-sort-order (quote recently-active))
- '(recentf-auto-cleanup (quote never))
+   '(go-mode ace-window frog-jump-buffer yasnippet-snippets rainbow-delimiters yaml-mode restclient markdown-mode rust-mode realgud realgud-lldb php-mode yasnippet projectile-ripgrep js2-mode coffee-mode robe haml-mode solarized-theme ag avy cider company-quickhelp counsel diminish etags-select macrostep magit nav-flash paredit projectile smex smooth-scrolling undo-tree use-package web-mode wgrep-ag which-key yesql-ghosts))
+ '(projectile-sort-order 'recently-active)
+ '(recentf-auto-cleanup 'never)
  '(recentf-max-saved-items nil)
  '(solarized-high-contrast-mode-line t)
  '(solarized-italic nil)
