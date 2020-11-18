a2b1285aef632777c02cdca94a21bec2c408be48
Jonathon McKitrick <jmckitrick@gmail.com>
2020-11-18 15:39:11 -0500

    Clojure mode tweaks

--- elisp/init-clojure.el
+++ elisp/init-clojure.el
@@ -14,13 +14,23 @@
    cider-prompt-save-file-on-load nil
    cider-repl-display-help-banner nil))
 
-(defun my-cider-repl-mode-hook ()
-  ;(company-mode 1)
-  ;(cider-eldoc-setup)
+(defun my-cider-mode-hook ()
+  ;;(company-mode 1)
+  (eldoc-mode 1)
+  (cider-eldoc-setup)
+  (company-mode)
   (use-package init-lisp)
   (my-lisp-setup))
 
-;(add-hook 'cider-mode-hook #'my-cider-mode-hook)
+(defun my-cider-repl-mode-hook ()
+  ;;(company-mode 1)
+  (eldoc-mode 1)
+  (cider-eldoc-setup)
+  (company-mode)
+  (use-package init-lisp)
+  (my-lisp-setup))
+
+(add-hook 'cider-mode-hook #'my-cider-mode-hook)
 (add-hook 'cider-repl-mode-hook #'my-cider-repl-mode-hook)
 
 (provide 'init-clojure)
