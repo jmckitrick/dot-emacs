5cf0e9a880aaca001d19d7563f38a1a5af25ebe3
Jonathon McKitrick <jmckitrick@gmail.com>
2020-12-11 08:18:43 -0500

    Personal settings

--- customize.el
+++ customize.el
@@ -15,27 +15,29 @@
  '(cider-repl-pop-to-buffer-on-connect nil)
  '(clojure-indent-style 'align-arguments)
  '(custom-safe-themes
-   '("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
+   '("00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" default))
  '(kill-whole-line t)
+ '(lsp-solargraph-use-bundler t)
+ '(lsp-sorbet-use-bundler t)
  '(magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
  '(magit-visit-ref-behavior '(checkout-branch))
  '(org-agenda-span 'day)
  '(package-selected-packages
-   '(browse-at-remote smartparens go-mode ace-window frog-jump-buffer yasnippet-snippets rainbow-delimiters yaml-mode restclient markdown-mode rust-mode realgud realgud-lldb php-mode yasnippet projectile-ripgrep js2-mode coffee-mode robe haml-mode solarized-theme ag avy cider company-quickhelp counsel diminish etags-select macrostep magit nav-flash paredit projectile smex smooth-scrolling undo-tree use-package web-mode wgrep-ag which-key yesql-ghosts))
+   '(lsp-ivy flycheck lsp-mode lsp-ui browse-at-remote smartparens go-mode ace-window frog-jump-buffer yasnippet-snippets rainbow-delimiters yaml-mode restclient markdown-mode rust-mode realgud realgud-lldb php-mode yasnippet projectile-ripgrep js2-mode coffee-mode robe haml-mode solarized-theme ag avy cider company-quickhelp counsel diminish etags-select macrostep magit nav-flash paredit projectile smex smooth-scrolling undo-tree use-package web-mode wgrep-ag which-key yesql-ghosts))
  '(projectile-sort-order 'recently-active)
  '(recentf-auto-cleanup 'never)
  '(recentf-max-saved-items nil)
  '(solarized-high-contrast-mode-line t)
  '(solarized-italic nil)
  '(tooltip-mode nil)
- '(visible-bell t)
- )
+ '(visible-bell t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+
  '(frog-menu-action-keybinding-face ((t (:inherit default :height 1.4))))
  '(frog-menu-actions-face ((t (:inherit default :height 1.4))))
  '(frog-menu-border ((t (:background "white" :height 1.4))))
--- elisp/init-consult.el
+++ elisp/init-consult.el
@@ -8,10 +8,19 @@
 
 (add-to-list 'auto-mode-alist '("\\.haml" . haml-mode))
 
-(add-hook 'ruby-mode-hook 'robe-mode)
+;; (add-hook 'ruby-mode-hook 'robe-mode)
 
-(eval-after-load 'company
-  '(push 'company-robe company-backends))
+;; (eval-after-load 'company
+;;   '(push 'company-robe company-backends))
+
+(use-package lsp-mode
+  :hook (
+         (ruby-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
 
 ;; php
 (add-to-list 'auto-mode-alist '("\\.php" . php-mode))
