570049e34248b5500dc66a3f60505878e4811efc
Jonathon McKitrick <jonathon.mckitrick@doublethedonation.com>
2021-08-07 07:15:29 -0400

    Work settings

--- elisp/init-work.el
+++ elisp/init-work.el
@@ -20,6 +20,26 @@
 (add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.mjml" . web-mode))
 
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
 (add-hook 'before-save-hook 'cider-format-buffer t t)
 
 (provide 'init-work)
