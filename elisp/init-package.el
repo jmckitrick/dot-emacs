a56ae3b1b7dc6e260065ceef0ad60dc9f808c460
Jonathon McKitrick <jmckitrick@gmail.com>
2021-11-11 20:21:53 -0500

    melpa setup

--- elisp/init-package.el
+++ elisp/init-package.el
@@ -3,10 +3,6 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-;;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
-;;(add-to-list 'package-pinned-packages '(projectile . "melpa-stable") t)
 
 (provide 'init-package)
