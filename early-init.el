acf4ad1a1f87b814be03d2517d68d3f1796d7eba
Jonathon McKitrick <jmckitrick@gmail.com>
2021-12-19 09:40:32 -0500

    Early init - load correct library path for system

--- early-init.el
+++ early-init.el
@@ -3,9 +3,16 @@
 (eval-when-compile (require 'subr-x))
 
 ;; Set up lib path for native compilation.
-(setenv "LIBRARY_PATH"
+(if (string-match "jcm-mbp" (system-name))
+    (setenv "LIBRARY_PATH"
+        (string-join
+         '("/opt/homebrew/Cellar/gcc/11.2.0_3/lib/gcc/11/"
+           "/opt/homebrew/Cellar/libgccjit/11.2.0_1/lib/gcc/11"
+           "/opt/homebrew/Cellar/gcc/11.2.0_3/lib/gcc/11/gcc/aarch64-apple-darwin21/11")
+         ":"))
+  (setenv "LIBRARY_PATH"
         (string-join
          '("/usr/local/opt/gcc/lib/gcc/11"
            "/usr/local/opt/libgccjit/lib/gcc/11/"
            "/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin20/11.2.0")
-         ":"))
+         ":")))
