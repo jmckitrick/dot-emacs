eea272d323bcc19f50656d3d91a0522822090c9f
Jonathon McKitrick <jmckitrick@gmail.com>
2022-02-20 20:53:52 -0500

    Common
    - update sheet
    - emmet mode
    - tone down hl line

--- elisp/init-misc.el
+++ elisp/init-misc.el
@@ -17,6 +17,7 @@
 ;; (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 ;; (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . emmet-mode))
 (setq web-mode-markup-indent-offset 2)
 
 (use-package magit
--- elisp/init-package.el
+++ elisp/init-package.el
@@ -3,6 +3,6 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (provide 'init-package)
