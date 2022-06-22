f1233e80cc166b96298ea243b2bb49683f4d86fa
Jonathon McKitrick <jmckitrick@gmail.com>
2022-06-22 17:20:00 -0400

    Fix native comp upgrade

--- early-init.el
+++ early-init.el
@@ -5,7 +5,7 @@
 ;; Set up lib path for native compilation.
 (setenv "LIBRARY_PATH"
         (string-join
-         '("/opt/homebrew/Cellar/gcc/11.3.0_1/lib/gcc/11/"
-           "/opt/homebrew/Cellar/libgccjit/11.3.0_1/lib/gcc/11"
-           "/opt/homebrew/Cellar/gcc/11.3.0_1/lib/gcc/11/gcc/aarch64-apple-darwin21/11")
+         '("/opt/homebrew/Cellar/gcc/11.3.0_2/lib/gcc/11/"
+           "/opt/homebrew/Cellar/libgccjit/11.3.0_2/lib/gcc/11"
+           "/opt/homebrew/Cellar/gcc/11.3.0_2/lib/gcc/11/gcc/aarch64-apple-darwin21/11")
          ":"))
