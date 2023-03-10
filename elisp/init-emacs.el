257865d094a19e52ca1bf4d921fb42ec8be08bab
Jonathon McKitrick <jmckitrick@gmail.com>
2023-03-10 07:47:08 -0500

    Try no transient mark mode

--- elisp/init-emacs.el
+++ elisp/init-emacs.el
@@ -23,7 +23,7 @@
 (setq inhibit-splash-screen t)
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode 0))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode 0))
-;;(transient-mark-mode -1)
+(transient-mark-mode -1)
 (setq use-dialog-box nil)
 
 ;; Editing settings.
