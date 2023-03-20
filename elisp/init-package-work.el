dbe3c7f795d3588f6d7f03c0197dfd5f36e5f5fc
Jonathon McKitrick <jonathon.mckitrick@doublethedonation.com>
2023-03-20 06:53:56 -0400

    Save work settings

--- elisp/init-package-work.el
+++ elisp/init-package-work.el
@@ -3,10 +3,11 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 ;;(add-to-list 'package-pinned-packages '(modus-themes . "melpa-stable"))
-;;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+;(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+;(add-to-list 'package-pinned-packages '(magit . "melpa-stable") t)
 ;;(add-to-list 'package-pinned-packages '(projectile . "melpa-stable") t)
 
 (provide 'init-package-work)
