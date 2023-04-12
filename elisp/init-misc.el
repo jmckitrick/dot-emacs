8639a36b635238b4e74968339242d6f6e7033ddd
Jonathon McKitrick <jonathon.mckitrick@doublethedonation.com>
2023-04-12 06:29:40 -0400

    Disable gc tweaks for now

--- elisp/init-misc.el
+++ elisp/init-misc.el
@@ -74,7 +74,7 @@
 (diminish 'auto-revert-mode)
 (diminish 'eldoc-mode)
 
-(setq gc-cons-threshold (eval-when-compile (* 1024 1024 1024)))
-(run-with-idle-timer 2 t (lambda () (garbage-collect)))
+;(setq gc-cons-threshold (eval-when-compile (* 1024 1024 1024)))
+;(run-with-idle-timer 10 t (lambda () (garbage-collect)))
 
 (provide 'init-misc)
