ebf37378dd13b8d87fbe20db679f0e4924471cb2
Jonathon McKitrick <jmckitrick@gmail.com>
2023-05-06 11:02:03 -0400

    Local stuff

--- elisp/init-package.el
+++ elisp/init-package.el
@@ -3,8 +3,8 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 ;;(add-to-list 'package-pinned-packages '(modus-themes . "melpa-stable"))
 
 (provide 'init-package)
